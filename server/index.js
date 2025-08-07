79a43e73dce34e187edadf55bf7a45deeb89b725
kgolder92 <golderkayla@gmail.com>
2025-08-07 16:16:38 -0400

    add Place Photo api on server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { response } from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import { Readable } from "stream";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -14,11 +15,12 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("/restaurants/test", (req, res) => {
-  console.log("*** IN SERVER ROUTE ***");
-  res.json({ ok: true });
-});
+// app.get("/restaurants/test", (req, res) => {
+//   console.log("*** IN SERVER ROUTE ***");
+//   res.json({ ok: true });
+// });
 
+//Nearby Search by default
 app.post("/api/restaurants/nearby", async (req, res) => {
   const { lat, lng } = req.body;
   const API_KEY = process.env.GOOGLE_PLACES_API_KEY;
@@ -29,7 +31,7 @@ app.post("/api/restaurants/nearby", async (req, res) => {
 
   try {
     const resp = await fetch(
-      `https://places.googleapis.com/v1/places:searchNearby?key=${API_KEY}&fields=places.displayName,places.location`,
+      `https://places.googleapis.com/v1/places:searchNearby?key=${API_KEY}&fields=places.displayName,places.location,places.rating,places.userRatingCount,places.formattedAddress,places.photos`,
       {
         method: "POST",
         headers: {
@@ -52,12 +54,43 @@ app.post("/api/restaurants/nearby", async (req, res) => {
       }
     );
     const data = await resp.json();
-    console.log("GOOGLE API STATUS:", resp.status);
-    console.log("GOOGLE API RESPONSE:", data);
+    console.log("GOOGLE API NEARBY RESPONSE:", data);
     res.json(data);
   } catch (err) {
     console.error("API error", err);
     res.status(500).json({ error: "Failed to fetch restaurants" });
+  }
+});
+
+app.get("/photos/:photoRef", async (req, res) => {
+  console.log("*** IN /photos ***");
+  const { photoRef } = req.params;
+  const API_KEY = process.env.GOOGLE_PLACES_API_KEY;
+
+  try {
+    const response = await fetch(
+      `https://places.googleapis.com/v1/${photoRef}/media?key=${API_KEY}&maxHeightPx=200&maxWidthPx=200`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        redirect: "follow",
+      }
+    );
+
+    if (!response.ok) {
+      return res
+        .status(response.status)
+        .json({ error: "Failed to fetch photo " });
+    }
+    console.log("GOOGLE API PHOTO RESPONSE:", res);
+
+    const readable = Readable.fromWeb(response.body);
+    readable.pipe(res);
+  } catch (err) {
+    console.error("Photo fetch error: ", err);
+    res.status(500).json({ error: "Photo fetch failed" });
   }
 });
 
